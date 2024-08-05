22a29360e12564ac499b38a384256ceaf3376d75
Huiming <610416981@qq.com>
2024-08-06 00:11:11 +0800

    添加了对compress时删除wal导致数据丢失的验证

--- example/main.go
+++ example/main.go
@@ -36,21 +36,59 @@ func main() {
 
 }
 
+func testResetWalBug() {
+	defer func() {
+		r := recover()
+		if r != nil {
+			fmt.Println(r)
+			inputReader := bufio.NewReader(os.Stdin)
+			_, _ = inputReader.ReadString('\n')
+		}
+	}()
+	lsm.Start(config.Config{
+		DataDir:       "./data",
+		Level0Size:    100,
+		PartSize:      4,
+		Threshold:     9,
+		CheckInterval: 2,
+	})
+	// 注释掉以下五行代码，再次运行以检验wal中新数据是否被删除
+	// insert 10 to start compress
+	insertValuesByCount(10, 10)
+	// wait for compress and insert kvs when compress begins
+	time.Sleep(2 * time.Second)
+	insertValuesByCount(6, 0)
+	keys := []string{"0", "1", "2", "3", "4", "5"}
+	queryByKeys(keys)
+	time.Sleep(10 * time.Second)
+}
+
+func queryByKeys(keys []string) {
+	for _, key := range keys {
+		start := time.Now()
+		v, _ := lsm.Get[TestValue](key)
+		elapse := time.Since(start)
+		fmt.Println("查找", key, " 完成，消耗时间：", elapse)
+		fmt.Println(v)
+	}
+}
+
 func query() {
 	start := time.Now()
-	v, _ := lsm.Get[TestValue]("aaaaaa")
+	v, _ := lsm.Get[TestValue]("4")
 	elapse := time.Since(start)
 	fmt.Println("查找 aaaaaa 完成，消耗时间：", elapse)
 	fmt.Println(v)
 
 	start = time.Now()
-	v, _ = lsm.Get[TestValue]("aazzzz")
+	v, _ = lsm.Get[TestValue]("2")
 	elapse = time.Since(start)
 	fmt.Println("查找 aazzzz 完成，消耗时间：", elapse)
 	fmt.Println(v)
 }
-func insert() {
 
+func insertValuesByCount(count, startFrom int) {
+	start := time.Now()
 	// 64 个字节
 	testV := TestValue{
 		A: 1,
@@ -58,7 +96,22 @@ func insert() {
 		C: 3,
 		D: "00000000000000000000000000000000000000",
 	}
-	
+	for i := 0; i < count; i++ {
+		lsm.Set(fmt.Sprint(i+startFrom), testV)
+	}
+	elapse := time.Since(start)
+	fmt.Println("插入完成，数据量：", count, ",消耗时间：", elapse)
+}
+
+func insert() {
+	// 64 个字节
+	testV := TestValue{
+		A: 1,
+		B: 1,
+		C: 3,
+		D: "00000000000000000000000000000000000000",
+	}
+
 	//testVData, _ := json.Marshal(testV)
 	//// 131 个字节
 	//kvData, _ := kv.Encode(kv.Value{
